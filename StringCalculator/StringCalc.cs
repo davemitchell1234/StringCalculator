81e14e848abc98d280d841ebed61e01ce069404c
Dave Mitchell <dave.mitchell@spikeisland.net>
2021-11-14 16:23:04 +0000

    Initial coding

---
+++ StringCalculator/StringCalc.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class StringCalc
+    {
+        private const string _commaDelimiter = ",";
+        private const string _newlineDelimiter = "\n";
+
+        public int Add(string numbers)
+        {
+            var delimiters = new List<string> { _commaDelimiter, _newlineDelimiter };
+            var inputValue = numbers;
+
+            inputValue = CheckForDelimiterInput(numbers, delimiters, inputValue);
+
+            var parts = inputValue.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            if (parts.Count < 3)
+            {
+                return 0;
+            }
+            else
+            {
+                return AnalyseInputNumbers(parts);
+            }
+        }
+
+        private string CheckForDelimiterInput(string numbers, List<string> delimiters, string inputValue)
+        {
+            if (numbers.Length > 4 && numbers.StartsWith("//"))
+            {
+                var firstNewLine = inputValue.IndexOf("\n");
+
+                switch (firstNewLine)
+                {
+                    case -1:
+                    case 0:
+                    case 1:
+                    case 2:
+                        // No delimiters - drop out and return inputValue
+                        break;
+
+                    case 3:
+                        // Single one character delimiter
+                        delimiters.Add(numbers.Substring(2, 1));
+                        inputValue = numbers.Substring(4);
+                        break;
+
+                    default:
+                        // Multiple delimiters of varying lengths
+                        var delimiterString = numbers.Substring(2, firstNewLine - 2);
+
+                        var delimitersSplit = delimiterString.Split(new string[] { "[", "]" }, 
+                                                                    StringSplitOptions.RemoveEmptyEntries)
+                                                                    .ToList();
+
+                        if (delimitersSplit.Any())
+                        {
+                            delimiters.AddRange(delimitersSplit);
+                            inputValue = numbers.Substring(firstNewLine + 1);
+                        }
+                        break;
+                }
+            }
+
+            return inputValue;
+        }
+
+        private int AnalyseInputNumbers(List<string> parts)
+        {
+            var result = 0;
+
+            var negativeNumbers = new List<int>();
+
+            parts.ForEach(s =>
+            {
+                if (int.TryParse(s, out var number))
+                {
+                    if (number < 0)
+                    {
+                        negativeNumbers.Add(number);
+                    }
+                    else if (number <= 1000)
+                    {
+                        result += number;
+                    }
+                }
+            });
+
+            if (negativeNumbers.Any())
+            {
+                throw new ApplicationException($"negatives not allowed - {string.Join(",", negativeNumbers)}");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ StringCalculatorTests/StringCalcTests.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace StringCalculator.Tests
+{
+    [TestClass()]
+    public class StringCalcTests
+    {
+        private StringCalc _sut;
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            _sut = new StringCalc();
+        }
+
+        [TestMethod()]
+        public void CallAddWithEmptyStringReturnsZero()
+        {
+            // Arrange
+            var args = "";
+
+            // Act
+            var result = _sut.Add(args);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void CallAddWithOneParameterReturnsZero()
+        {
+            // Arrange
+            var args = "1";
+
+            // Act
+            var result = _sut.Add(args);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void CallAddWithTwoParametersReturnsZero()
+        {
+            // Arrange
+            var args = "1,2";
+
+            // Act
+            var result = _sut.Add(args);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [DataTestMethod()]
+        [DataRow("1,2,3", 6)]
+        [DataRow("4,5,6", 15)]
+        public void CallAddWithThreeParametersReturnsCorrectSum(string inputNumbers, int expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var result = _sut.Add(inputNumbers);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod()]
+        public void CallAddWithLargeNumberOfParametersReturnsCorrectSum()
+        {
+            // Arrange
+            var args = "1,2,3,6,7,12,15,36,45,67";
+
+            // Act
+            var result = _sut.Add(args);
+
+            // Assert
+            Assert.AreEqual(194, result);
+        }
+
+        [TestMethod()]
+        public void CallAddWithNewlineDelimiterReturnsCorrectSum()
+        {
+            // Arrange
+            var args = "1\n2,3";
+
+            // Act
+            var result = _sut.Add(args);
+
+            // Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod()]
+        public void CallAddWithCustomDelimiterReturnsCorrectSum()
+        {
+            // Arrange
+            var args = "//;\n1;2;3";
+
+            // Act
+            var result = _sut.Add(args);
+
+            // Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod()]
+        public void CallAddWithNegativeNumbersThrowsException()
+        {
+            // Arrange
+            var args = "1,2,3,-4,5,-6";
+
+            // Act & Assert
+            var ex = Assert.ThrowsException<ApplicationException>(() => _sut.Add(args));
+
+            Assert.AreEqual("negatives not allowed - -4,-6", ex.Message);
+        }
+
+        [TestMethod()]
+        public void CallAddWithNumbersGreaterThan1000AreIgnored()
+        {
+            // Arrange
+            var args = "1,2,3,1000,1001";
+
+            // Act
+            var result = _sut.Add(args);
+
+            // Assert
+            Assert.AreEqual(1006, result);
+        }
+
+        [TestMethod()]
+        public void CallAddWithCustomDelimiterAnyLengthReturnsCorrectSum()
+        {
+            // Arrange
+            var args = "//[***]\n1***2***3";
+
+            // Act
+            var result = _sut.Add(args);
+
+            // Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod()]
+        public void CallAddWithMultipleSingleCharacterCustomDelimitersReturnsCorrectSum()
+        {
+            // Arrange
+            var args = "//[*][%]\n1*2%3";
+
+            // Act
+            var result = _sut.Add(args);
+
+            // Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod()]
+        public void CallAddWithMultipleCustomDelimitersOfVaryingLengthsReturnsCorrectSum()
+        {
+            // Arrange
+            var args = "//[**][%%%]\n1**2%%%3";
+
+            // Act
+            var result = _sut.Add(args);
+
+            // Assert
+            Assert.AreEqual(6, result);
+        }
+    }
+}
